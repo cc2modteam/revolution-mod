82059f2363a1892539649cc645710d50fd4a2206
inb <inorton@gmail.com>
2024-05-24 22:14:54 +0100

    RWR tones for the Manta

--- content/scripts/screen_vehicle_camera.lua
+++ content/scripts/screen_vehicle_camera.lua
@@ -10,11 +10,91 @@ function begin()
     g_region_icon = begin_get_ui_region_index("microprose")
 end
 
+g_rwr_last_tick = 0
+g_rwr_last_nails = 0
+g_rwr_nails_interval = 7 * 30
+g_rwr_spike_interval = 1 * 30
+g_spike_tone_count = 0
+g_nail_tone_count = 0
+
+
+function play_nails_tone()
+    update_play_sound(e_audio_effect_type.telemetry_5)
+    update_play_sound(e_audio_effect_type.telemetry_5)
+    update_play_sound(e_audio_effect_type.telemetry_5)
+    update_play_sound(e_audio_effect_type.telemetry_5)
+    update_play_sound(e_audio_effect_type.telemetry_5)
+end
+
+function play_tracking_tone()
+    if g_nail_tone_count % 3 == 0 then
+        play_nails_tone()
+    elseif g_nail_tone_count % 3 == 1 then
+        play_nails_tone()
+    end
+    g_nail_tone_count = g_nail_tone_count + 1
+end
+
+function play_spike_tone()
+    g_nail_tone_count = g_nail_tone_count + 1
+    play_nails_tone()
+end
+
+
+function aircraft_rwr_sfx(vehicle)
+    -- dont render unless we are the seat
+    local self_peer = update_get_local_peer_id()
+    local pilot_peer = vehicle:get_controlling_peer_id()
+    if self_peer ~= pilot_peer then
+        return
+    end
+
+
+    local now = update_get_logic_tick()
+    local prev = g_rwr_last_tick
+    if now > prev + g_rwr_spike_interval then
+        g_rwr_last_tick = now
+        refresh_modded_radar_cache()
+        local radar, dist = get_nearest_hostile_radar(vehicle:get_id())
+        if radar and dist < get_rwr_range() then
+            if dist < 10000 then
+                -- spike - double chirp
+                if dist > 5000 then
+                    play_tracking_tone()
+                else
+                    play_spike_tone()
+                end
+                return
+            else
+                if now > g_rwr_last_nails + g_rwr_nails_interval then
+                    -- nails
+                    play_nails_tone()
+                    g_rwr_last_nails = now
+                end
+            end
+        end
+    elseif prev + g_rwr_nails_interval < now then
+        local nails = vehicle:get_is_visible_by_enemy()
+        if nails then
+            update_play_sound(e_audio_effect_type.telemetry_5)
+            update_play_sound(e_audio_effect_type.telemetry_5)
+            update_play_sound(e_audio_effect_type.telemetry_5)
+            return
+        end
+    end
+end
+
 function update(screen_w, screen_h, ticks) 
     update_set_screen_background_type(0)
     if update_screen_overrides(screen_w, screen_h, ticks)  then return end
 
     local screen_vehicle = update_get_screen_vehicle()
+    if screen_vehicle and screen_vehicle:get() then
+        if get_has_rwr(screen_vehicle) then
+            aircraft_rwr_sfx(screen_vehicle)
+            return
+        end
+    end
 
     update_set_screen_background_type(9)
     update_set_screen_camera_pos_orientation(g_cam_x, g_cam_y, g_cam_z, g_cam_rot)
