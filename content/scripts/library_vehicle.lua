dc3b083efb8b9d464d9ee9c679178f92a0fdcaef
Ian Norton <inorton@gmail.com>
2025-12-27 14:20:00 +0000

    Allow Walrus to carry a more limited AA suite Allow Bear to carry TV/AA

--- content/scripts/library_vehicle.lua
+++ content/scripts/library_vehicle.lua
@@ -3148,48 +3148,50 @@ local st, _v = pcall(function()
         -- bear
         [e_game_object_type.chassis_land_wheel_heavy] = {
             options = {
-                [3] = concat_lists(_std_wing_utils, {
-                    e_game_object_type.attachment_camera,
+                [4] = {
+                    e_game_object_type.attachment_hardpoint_missile_aa,
                     e_game_object_type.attachment_hardpoint_missile_tv,
-                }),
-                [1] = concat_lists(_std_wing_utils, {
-                    e_game_object_type.attachment_camera,
-                    e_game_object_type.attachment_hardpoint_missile_tv,
-                })
-            },
-        },
-        -- petrel
-        [e_game_object_type.chassis_air_rotor_heavy] = {
-            options = {
-                -- nose slot
-                [1] = {
-                    e_game_object_type.attachment_camera_plane,
-                    e_game_object_type.attachment_turret_gimbal_30mm,
-                    e_game_object_type.attachment_radar_golfball,
                 },
-               -- [7] = {
-               --     e_game_object_type.attachment_deployable_droid,
-               -- },
-               -- [8] = {
-               --     e_game_object_type.attachment_smoke_launcher_explosive,
-               -- }
+                [5] = {
+                    e_game_object_type.attachment_hardpoint_missile_aa,
+                    e_game_object_type.attachment_hardpoint_missile_tv,
+                }
             },
             rows = {
                 {
-                    { i=1, x=0, y=-22 }
-                },
-                {
-                    { i=2, x=-23, y=0 },
-                    { i=4, x=-14, y=0 },
-                    { i=5, x=14, y=0 },
-                    { i=3, x=23, y=0 }
-                },
-                {
-                --    { i=7, x=0, y=10},
-                    { i=8, x=0, y=23}
+                    {i=1, x=-16, y=0},
+                    {i=2, x=0, y=-6},
+                    {i=3, x=16, y=0},
+                    {i=4, x=-8, y=12},
+                    {i=5, x=8, y=12},
                 }
-            }
+            },
         },
+        ---- petrel
+        --[e_game_object_type.chassis_air_rotor_heavy] = {
+        --    options = {
+        --        -- nose slot
+        --        [1] = {
+        --            e_game_object_type.attachment_camera_plane,
+        --            e_game_object_type.attachment_turret_gimbal_30mm,
+        --        },
+        --    },
+        --    rows = {
+        --        {
+        --            { i=1, x=0, y=-22 }
+        --        },
+        --        {
+        --            { i=2, x=-23, y=0 },
+        --            { i=4, x=-14, y=0 },
+        --            { i=5, x=14, y=0 },
+        --            { i=3, x=23, y=0 }
+        --        },
+        --        {
+        --        --    { i=7, x=0, y=10},
+        --            { i=8, x=0, y=23}
+        --        }
+        --    }
+        --},
         -- turret
         [e_game_object_type.chassis_land_turret] = {
             options = {
